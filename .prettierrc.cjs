535f1b75cc8ddad366bcbc582dca5883bac0eaaa
Leire Ordeñana Madina <leireomadina@gmail.com>
2026-02-21 12:40:19 +0100

    Add prettier config and dependencies

---
+++ .prettierrc.cjs
@@ -0,0 +1,19 @@
+module.exports = {
+    trailingComma: 'es5',
+    useTabs: true,
+    tabWidth: 2,
+    semi: false,
+    singleQuote: true,
+    endOfLine: 'auto',
+    plugins: [
+        'prettier-plugin-astro',
+    ],
+    overrides: [
+        {
+            files: '**/*.astro',
+            options: {
+                parser: 'astro',
+            },
+        },
+    ],
+}
