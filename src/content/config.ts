f25ca76b64bdac83f22b9041083c77d9987a3b51
Leire Ordeñana Madina <leireomadina@gmail.com>
2026-03-14 12:21:17 +0100

    Define astro collections: albums and photos

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,30 +1,31 @@
 import { defineCollection } from 'astro:content'
-import { glob, file } from 'astro/loaders'
+import { glob } from 'astro/loaders'
 import { z } from 'astro/zod'
 
 const albums = defineCollection({
-    loader: glob({ pattern: ['*.md'], base: 'src/content/albums' }),
-    schema: z.object({
-        title: z.string(),
-        description: z.string().optional(),
-        type: z.enum(['trip', 'place', 'theme']),
-        cover: z.string(),
-        date: z.string(),
-    })
+	loader: glob({ pattern: ['*.md'], base: 'src/content/albums' }),
+	schema: ({ image }) =>
+		z.object({
+			title: z.string(),
+			description: z.string().optional(),
+			type: z.enum(['trip', 'place', 'theme']),
+			date: z.string(),
+			cover: image().optional(),
+		}),
 })
 
 const photos = defineCollection({
-    loader: glob({ pattern: ['**/*.md'], base: 'src/content/photos' }),
-    schema: z.object({
-        title: z.string(),
-        description: z.string().optional(),
-        image: z.string(),
-        collection: z.string(),
-        favorite: z.boolean().default(false),
-        date: z.date(),
-        type: z.enum(['architecture', 'portrait', 'landscape']).optional(),
-    })
-
+	loader: glob({ pattern: ['**/*.md'], base: 'src/content/photos' }),
+	schema: ({ image }) =>
+		z.object({
+			title: z.string(),
+			description: z.string().optional(),
+			image: image(),
+			collection: z.string(),
+			favorite: z.boolean().default(false),
+			date: z.date(),
+			type: z.enum(['architecture', 'portrait', 'landscape']).optional(),
+		}),
 })
 
-export const collections = { albums, photos };
+export const collections = { albums, photos }
